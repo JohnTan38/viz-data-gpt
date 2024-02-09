1b943972bdff16ccf63d6c5c050e791c4d43ec7e
JohnTan38 <113171463+JohnTan38@users.noreply.github.com>
2024-02-09 21:15:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://ask-csv.streamlit.app/)
+
+# Ask Your Data (GPT-powered)
+
+A web app that allows to interact with user-uploaded data in .csv.
+
+-  Ask questions about your data, such as "what was the total sales in the US in 2022?"
+-  Visualize data from your csv file using natural language. For example, "plot total sales by country and product category"
+
+
+Upload a csv file with data:
+
+<img src="https://i.postimg.cc/htTtJfwV/gif-upload.gif"/>
+
+Write a question to your data:
+
+<img src="https://i.postimg.cc/T33XxhxR/gif-query.gif"/>
+
+Create a visualization:
+
+<img src="https://i.postimg.cc/SxMfJfkV/gif-viz.gif"/>
---
+++ Scripts/main.py
@@ -0,0 +1,219 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+from sqlite3 import Connection
+import openai
+import plotly.express as px
+import plotly.graph_objs as go
+import numpy as np
+import re
+from dateutil.parser import parse
+import traceback
+
+
+footer_html = """
+    <div class="footer">
+    <style>
+        .footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background-color: #f0f2f6;
+            padding: 10px 20px;
+            text-align: center;
+        }
+        .footer a {
+            color: #4a4a4a;
+            text-decoration: none;
+        }
+        .footer a:hover {
+            color: #3d3d3d;
+            text-decoration: underline;
+        }
+    </style>
+        Connect with me <a href="mailto:vieming@hotmail.com" target="_blank">vieming@hotmail.com</a>. 
+        If you like this app, consider <a href="https://www.buymeacoffee.com/vieming8" target="_blank">buying me a coffee</a> ☕
+    </div>
+"""
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
+
+def create_connection(db_name: str) -> Connection:
+    conn = sqlite3.connect(db_name)
+    return conn
+
+def run_query(conn: Connection, query: str) -> pd.DataFrame:
+    df = pd.read_sql_query(query, conn)
+    print(df)
+    return df
+
+def create_table(conn: Connection, df: pd.DataFrame, table_name: str):
+    df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+
+def generate_gpt_reponse(gpt_input, max_tokens):
+
+    # load api key from secrets / get api key from input
+    #openai.api_key = st.secrets["openai_api_key"]
+    openai.api_key = openai_api_key
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=max_tokens,
+        temperature=0,
+        messages=[
+            {"role": "user", "content": gpt_input},
+        ]
+    )
+
+    gpt_response = completion.choices[0].message['content'].strip()
+    print(gpt_response)
+    return gpt_response
+
+
+def extract_code(gpt_response):
+    """function to extract code and sql query from gpt response"""
+
+    if "```" in gpt_response:
+        # extract text between ``` and ```
+        pattern = r'```(.*?)```'
+        code = re.search(pattern, gpt_response, re.DOTALL)
+        extracted_code = code.group(1)
+
+        # remove python from the code (weird bug)
+        extracted_code = extracted_code.replace('python', '')
+        print(extracted_code)
+
+        return extracted_code
+    else:
+        return gpt_response
+
+
+# wide layout
+st.set_page_config(page_icon="🤖", page_title="Chat CSV")
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+st.title("CHAT CSV 🤖 (GPT4)")
+st.header('Use Natural Language to Query Your Data')
+
+with st.sidebar:
+    openai_api_key = st.text_input("YOUR_OPENAI_API_KEY", type="password")
+openai.api_key = openai_api_key
+
+
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+if uploaded_file is None:
+    st.info(f"""
+                👆 Upload a .csv file first. Sample to try: [sample_data.csv](https://docs.google.com/spreadsheets/d/e/2PACX-1vTeB7_jzJtacH3XrFh553m9ahL0e7IIrTxMhbPtQ8Jmp9gCJKkU624Uk1uMbCEN_-9Sf7ikd1a85wIK/pub?gid=0&single=true&output=csv)
+                """)
+
+elif uploaded_file:
+    df = pd.read_csv(uploaded_file)
+
+    # Apply the custom function and convert date columns
+    for col in df.columns:
+        # check if a column name contains date substring
+        if 'date' in col.lower():
+            df[col] = pd.to_datetime(df[col])
+            # remove timestamp
+            #df[col] = df[col].dt.date
+
+    # reset index
+    df = df.reset_index(drop=True)
+
+    # replace space with _ in column names
+    df.columns = df.columns.str.replace(' ', '_')
+
+    cols = df.columns
+    cols = ", ".join(cols)
+
+    with st.expander("Preview of the uploaded file"):
+        st.table(df.head())
+
+    conn = create_connection(":memory:")
+    table_name = "my_table"
+    create_table(conn, df, table_name)
+
+
+    selected_mode = st.selectbox("What do you want to do?", ["Chat with data", "Create a chart [beta]"])
+
+    if selected_mode == 'Chat with data':
+
+        user_input = st.text_area("Write a concise and clear question about your data. For example: What is the total sales in the USA in 2022?", value='What is the total sales in the USA in 2022?')
+
+        if st.button("Get Response"):
+
+            try:
+                # create gpt prompt
+                gpt_input = 'Write a sql lite query based on this question: {} The table name is my_table and the table has the following columns: {}. ' \
+                            'Return only a sql query and nothing else'.format(user_input, cols)
+
+                query = generate_gpt_reponse(gpt_input, max_tokens=200)
+                query_clean = extract_code(query)
+                result = run_query(conn, query_clean)
+
+                with st.expander("SQL query used"):
+                    st.code(query_clean)
+
+                # if result df has one row and one column
+                if result.shape == (1, 1):
+
+                    # get the value of the first row of the first column
+                    val = result.iloc[0, 0]
+
+                    # write one liner response
+                    st.subheader('Your response: {}'.format(val))
+
+                else:
+                    st.subheader("Your result:")
+                    st.table(result)
+
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+                #st.error('Oops, there was an error :( Please try again with a different question.')
+
+    elif selected_mode == 'Create a chart [beta]':
+
+        user_input = st.text_area(
+            "Briefly explain what you want to plot from your data. For example: Plot total sales by country and product category", value='Plot total sales by country and product category')
+
+        if st.button("Create a visualization [beta]"):
+            try:
+                # create gpt prompt
+                gpt_input = 'Write code in Python using Plotly to address the following request: {} ' \
+                            'Use df that has the following columns: {}. Do not use animation_group argument and return only code with no import statements, use transparent background, the data has been already loaded in a df variable'.format(user_input, cols)
+
+                gpt_response = generate_gpt_reponse(gpt_input, max_tokens=1500)
+
+                extracted_code = extract_code(gpt_response)
+
+                extracted_code = extracted_code.replace('fig.show()', 'st.plotly_chart(fig)')
+
+                with st.expander("Code used"):
+                    st.code(extracted_code)
+
+                # execute code
+                exec(extracted_code)
+
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+                #st.write(traceback.print_exc())
+                #st.error('Oops, there was an error :( Please try again with a different question.')
+# footer
+st.markdown(footer_html, unsafe_allow_html=True)
